47833895b85f9260855669b5725c89e3a4a78594
yap12138 <438773897@qq.com>
2018-12-02 18:22:25 +0800

    Change server.cpp for Hogason Exp5

--- example/server.cc
+++ example/server.cc
@@ -8,11 +8,13 @@
 #include <httplib.h>
 #include <cstdio>
 #include <chrono>
+#include "utils.h"
 
 #define SERVER_CERT_FILE "./cert.pem"
 #define SERVER_PRIVATE_KEY_FILE "./key.pem"
 
 using namespace httplib;
+using namespace hogason;
 
 std::string dump_headers(const Headers& headers)
 {
@@ -79,18 +81,55 @@ int main(void)
         return -1;
     }
 
-    svr.Get("/", [=](const Request& /*req*/, Response& res) {
-        res.set_redirect("/hi");
-    });
+	svr.Get("/", [=](const Request& req, Response& res) {
+		auto cookie_str = req.get_header_value("Cookie");
+		if (cookie_str.empty()) {
+			res.set_redirect("/login.html");
+			return;
+		}
 
-    svr.Get("/hi", [](const Request& /*req*/, Response& res) {
-        res.set_content("Hello World!\n", "text/plain");
-    });
+		Cookie cookie(cookie_str);
+		try
+		{
+			auto session_id = cookie.getValue(Cookie::KEY_SESSION_ID);
+			// invalidate expires
+			SessionHolder::getInstance().getSession(session_id);
+			std::string file_buf;
+			detail::read_file("www/yap/index.html", file_buf);
 
-    svr.Get("/slow", [](const Request& /*req*/, Response& res) {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        res.set_content("Slow...\n", "text/plain");
-    });
+			std::vector<char> buf(file_buf.size() + 20);
+			snprintf(&buf[0], buf.size(), file_buf.c_str(), session_id.c_str());
+			res.set_content(buf.data(), "text/html");
+		}
+		catch (std::runtime_error& /*ex*/)
+		{
+			// session over time
+			cookie.setExpires(system_clock::now() - hours(1));
+			res.set_header("Set-Cookie", cookie.to_string().c_str());
+			res.set_redirect("/login.html");
+			return;
+		}
+
+	});
+
+	svr.Post("/index", [](const Request& req, Response& res) {
+		auto username = req.get_param_value("userName");
+		auto password = req.get_param_value("password");
+		if (username.empty() || password.empty()) {
+			res.set_redirect("/login.html");
+			return;
+		}
+		// TODO validate info & generate session id
+
+		Cookie cookie({ { Cookie::KEY_SESSION_ID, username } });
+		SessionHolder::getInstance().setSession(username, cookie);
+		res.set_redirect("/");
+		res.set_header("Set-Cookie", cookie.to_string().c_str());
+	});
+
+	svr.Get("/index", [](const Request& /*req*/, Response& res) {
+		res.set_content("Hello World!\n", "text/plain");
+	});
 
     svr.Get("/dump", [](const Request& req, Response& res) {
         res.set_content(dump_headers(req.headers), "text/plain");
@@ -110,8 +149,28 @@ int main(void)
     svr.set_logger([](const Request& req, const Response& res) {
         printf("%s", log(req, res).c_str());
     });
+	
+	std::thread sessionKeeper([&svr]() {
+		SessionHolder& holder = SessionHolder::getInstance();
+		do
+		{
+			auto cur_t = std::chrono::system_clock::now();
+			auto session_ids = holder.getSessionIdSet();
+			for (const auto& id : session_ids) {
+				auto& session = holder.getSession(id);
+				if (session.create_t + SessionHolder::expires < cur_t) {
+					holder.removeSession(id);
+				}
+			}
 
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+		while (svr.is_running());
+	});
+
+	svr.set_base_dir("www");
     svr.listen("localhost", 8080);
+	sessionKeeper.join();
 
     return 0;
 }
